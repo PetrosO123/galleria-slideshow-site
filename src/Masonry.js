bcadaf82fd3ed07ad0a89c23af8f59b44d71e13f
PetrosO123 <pmo20@miami.edu>
2024-02-12 15:23:07 -0500

    galleria UI achieved

--- src/Masonry.js
+++ src/Masonry.js
@@ -1,14 +1,30 @@
 import "./App.css";
 import data from "./data.json";
+import { useState } from "react";
 
 function Masonry() {
-  console.log("data: ", data);
+  const [hovered, setHovered] = useState(false);
+  const handleHover = (i) => {
+    setHovered(i);
+  };
   return (
     <main className="masonry">
       {data.map((obj, i) => {
         return (
-          <div className="card" key={i}>
-            <img src={obj.images.thumbnail} alt={obj.description}></img>
+          <div
+            className="card"
+            key={i}
+            hovered={hovered}
+            onMouseOver={() => handleHover(i)}
+          >
+            <img src={obj.images.gallery} alt={obj.description}></img>
+            {hovered === i ? (
+              <div className="cardDetails">
+                <div className="cardName">{obj.name}</div>
+                <div className="cardArtist">{obj.artist.name}</div>
+              </div>
+            ) : null}
+            {hovered === i ? <div className="cardGradient"></div> : null}
           </div>
         );
       })}
