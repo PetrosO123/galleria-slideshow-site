4093ea0dd5ca5104330fcdb11b21ef1c0a7b0027
PetrosO123 <pmo20@miami.edu>
2024-02-17 11:47:12 -0500

    improved responsiveness on tablet and mobile devices, added modal functionality

--- src/CardPage.js
+++ src/CardPage.js
@@ -15,7 +15,22 @@ function CardPage({ data, entered, setEntered, progress, setProgress }) {
     setEntered(newEntered);
     setProgress(((newEntered + 1) / data.length) * 100);
   };
-  return (
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  // const [modalImage, setModalImage] = useState("");
+
+  // const openModalWithImage = (imageUrl) => {
+  //   console.log("modal open!");
+  //   console.log("imageURL: ", imageUrl);
+  //   setModalImage(`${process.env.PUBLIC_URL}/${imageUrl}`);
+  // };
+
+  const toggleModal = () => {
+    setIsModalOpen(!isModalOpen);
+    // openModalWithImage(`${data[entered].images.hero.large}`);
+  };
+  return entered == null ? (
+    <div>Loading...</div>
+  ) : (
     <div className="wrapper-cardPage">
       <div className="midsection">
         <div className="pageLeft">
@@ -25,23 +40,42 @@ function CardPage({ data, entered, setEntered, progress, setProgress }) {
               alt="arnolfini"
             />
           </div>
-
+          <div className="modalBtn" onClick={toggleModal}>
+            <img
+              src={`${
+                process.env.PUBLIC_URL
+              }/${`assets/shared/icon-view-image.svg`}`}
+              alt="view"
+            />
+            <div className="viewImage">VIEW IMAGE</div>
+          </div>
           <div className="work-artist">
             <div className="cardPageTitle">{data[entered].name}</div>
             <div className="cardPageSubtitle">{data[entered].artist.name}</div>
           </div>
-          <div className="artist">
-            <img
-              src={`${process.env.PUBLIC_URL}/${data[entered].artist.image}`}
-              alt={data[entered].artist.name}
-            />
-          </div>
+          <img
+            className="artist"
+            src={`${process.env.PUBLIC_URL}/${data[entered].artist.image}`}
+            alt={data[entered].artist.name}
+          />
         </div>
         <div className="pageRight">
           <div className="artDescription">{data[entered].description}</div>
           <div className="rightNumber">{data[entered].year}</div>
         </div>
       </div>
+      {isModalOpen ? (
+        <div className="modalOverlay" onClick={toggleModal}>
+          <div className="modalImageWrapper">
+            <img
+              src={`${process.env.PUBLIC_URL}/${data[entered].images.hero.large}`}
+              alt="Focused"
+              className="focusedImage"
+            />
+            <div className="close">CLOSE</div>
+          </div>
+        </div>
+      ) : null}
       <footer>
         <div className="bottom">
           <div className="progress" style={{ width: `${progress}%` }}></div>
